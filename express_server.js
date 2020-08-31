51ab671eb38831d1964b77f197a21e0fb5ac5773
Dani <dreamboy.dani@gmail.com>
2020-08-31 16:51:22 -0600

    Added new route shortURL and template urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -16,10 +16,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL]
+
+  const templateVars = {
+    shortURL, longURL
+  }
+
+  // console.log(req.params.shiort)
+  res.render('urls_show', templateVars)
+})
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
