3d7c661fa51d54f30d74b56ab9ecedb03256cd4b
Dani <dreamboy.dani@gmail.com>
2020-08-31 17:45:03 -0600

    Installed and initialized body-parser

--- express_server.js
+++ express_server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const { render } = require('ejs');
 const app = express();
 const PORT = 8080 // default port
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+
+function generateRandomString() {
+  console.log(Math.random().toString(36).substring(0, 7))
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,6 +26,15 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls/', (req, res) => {
+  console.log(req.body); // log the POST request's body to console.
+  res.send('OK') // Responds with the OK.
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL]
@@ -27,10 +42,10 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL, longURL
   }
-
-  // console.log(req.params.shiort)
   res.render('urls_show', templateVars)
 })
+
+
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
