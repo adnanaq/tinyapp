804f22836a49bb622b64c33cee557a8897abc7d0
Dani <dreamboy.dani@gmail.com>
2020-09-01 10:54:19 -0600

    Added the logic route and logic to delete the url

--- express_server.js
+++ express_server.js
@@ -3,12 +3,15 @@ const { render } = require('ejs');
 const app = express();
 const PORT = 8080 // default port
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // setting up the view engine with ejs
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
+// function to generate rando 6 character of ID to be used as key for the longURL.
 function generateRandomString() {
-  console.log(Math.random().toString(36).substring(0, 7))
+  return Math.random().toString(36).substring(2, 8)
 }
 
 const urlDatabase = {
@@ -21,28 +24,49 @@ app.get('/', (req, res) => {
   res.send('Hello!')
 })
 
+// route to the list of short and long URL list
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+// route to new URL search query
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
-app.post('/urls/', (req, res) => {
-  console.log(req.body); // log the POST request's body to console.
-  res.send('OK') // Responds with the OK.
-})
-
+// route to use shortURL to get to the longURL
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL]
+  const longURL = urlDatabase[shortURL];
+
 
   const templateVars = {
     shortURL, longURL
   }
   res.render('urls_show', templateVars)
+})
+
+// POST route to post the new url in the database
+app.post('/urls/', (req, res) => {
+
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+
+  urlDatabase[shortURL] = longURL
+
+  res.redirect(`/urls/${shortURL}`)
+})
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+})
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL]
+  res.redirect('/urls')
 })
 
 
